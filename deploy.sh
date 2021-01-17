5e01a53cd25305e11f3ad65d9fcd527e6fa27f6d
ii887522 <ii887522@gmail.com>
2021-01-17 17:37:06 +0800

    Fix Linux issues

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-docker run --rm --name hydro_publisher -it -w /hydro -v $PWD:/hydro node:15.4.0-alpine3.10 npm login && npm publish --access public
+docker run --rm --name hydro_publisher -it -w /hydro -v $PWD:/hydro node:15.4.0-alpine3.10 /bin/sh -c "npm login && npm publish --access public"
--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-docker run --rm --name hydro_tester -w /hydro -v $PWD:/hydro hydro-custom-node ncu -u && npm install && npx tsc && npm test
+docker run --rm --name hydro_tester -w /hydro -v $PWD:/hydro hydro-custom-node /bin/sh -c "chmod 777 /root && ncu -u && npm install && npx tsc && npm test"
