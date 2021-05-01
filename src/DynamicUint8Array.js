a4dfe919a6c577571a88f70231dd5ed85f5e5774
Chi Min Yong <chimin.yong@g2g.com>
2021-05-01 13:24:23 +0800

    Add DynamicUint8Array

---
+++ src/DynamicUint8Array.js
@@ -0,0 +1,29 @@
+'use strict';
+export default class DynamicUint8Array {
+    constructor() {
+        this.lData = new Uint8Array(32);
+        this.rData = new Uint8Array(0);
+        this.currentData = this.lData;
+        this.nextData = this.rData;
+        this.size = 0;
+    }
+    get() {
+        return this.currentData.slice(0, this.size);
+    }
+    add(array) {
+        if (this.size + array.length > this.currentData.length)
+            this.grow(Math.floor((this.size + array.length) * 1.33));
+        this.currentData.set(array, this.size);
+        this.size += array.length;
+    }
+    grow(nextSize) {
+        this.nextData = new Uint8Array(nextSize);
+        this.swapData();
+        this.currentData.set(this.nextData);
+    }
+    swapData() {
+        const aux = this.currentData;
+        this.currentData = this.nextData;
+        this.nextData = aux;
+    }
+}
